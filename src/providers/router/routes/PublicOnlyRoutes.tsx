e55774a63c17219737aae0f5ce3937d6168ee3ef
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-03-21 07:35:07 +0100

    PublicOnlyRoutes guard Feat

---
+++ src/providers/router/routes/PublicOnlyRoutes.tsx
@@ -0,0 +1,23 @@
+import React, { FC, lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Spinner from '@/atomes/Spinner/Spinner'
+import NotFound from '@/pages/NotFound'
+import { PublicOnlyLayout } from '@/providers/Layouts'
+
+const Auth = lazy(() => import('@/pages/auth'))
+
+const PublicOnlyRoutes: FC<React.PropsWithChildren<unknown>> = () => {
+	return (
+		<PublicOnlyLayout>
+			<Suspense fallback={<Spinner />}>
+				<Routes>
+					<Route index element={<Auth />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</Suspense>
+		</PublicOnlyLayout>
+	)
+}
+
+export default PublicOnlyRoutes
