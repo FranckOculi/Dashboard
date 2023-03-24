25e9e6b1b58260a3c670b026ef5db0f8692d474f
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-03-24 07:41:22 +0100

    PublicOnlyRoutes modify imports

--- src/providers/router/routes/PublicOnlyRoutes.tsx
+++ src/providers/router/routes/PublicOnlyRoutes.tsx
@@ -2,7 +2,7 @@ import React, { FC, lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Spinner from '@/atomes/Spinner/Spinner'
-import NotFound from '@/pages/NotFound'
+import NotFound from '@/pages/notFound'
 import { PublicOnlyLayout } from '@/providers/Layouts'
 
 const Auth = lazy(() => import('@/pages/auth'))
