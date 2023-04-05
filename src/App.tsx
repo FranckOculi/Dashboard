385f61211fda0f76deab0cc2decde8609b214a9e
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-04-05 07:30:37 +0200

    App update : add BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { useRoutes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { Providers } from '@/providers'
 
-import { routes } from './router/routes'
 import Layout from './pages/global/Layout'
 
 function App() {
-	const content = useRoutes(routes)
-
 	return (
-		<Providers>
-			<Layout children={content} />
-		</Providers>
+		<BrowserRouter>
+			<Providers>
+				<Layout />
+			</Providers>
+		</BrowserRouter>
 	)
 }
 
