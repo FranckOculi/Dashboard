fc0a6d15678cade97ec0284f61e6df0435d8e2a8
Chouchoubeignet <100290322+Chouchoubeignet@users.noreply.github.com>
2023-04-11 08:52:05 +0200

    Providers Update export

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useRoutes } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-import { Providers } from '@/providers'
-import Layout from './pages/global/Layout'
+import Providers from '@/providers'
+import Layout from '@/pages/global/Layout'
 
 function App() {
 	return (
